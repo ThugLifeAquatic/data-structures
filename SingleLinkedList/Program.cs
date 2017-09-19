e9ef8f78cbb9694f5710591204d193084cc6b9a6
Austin Cohrs <acohrsdev@outlook.com>
2017-09-18 18:28:49 -0700

    Completed single Linked List Challenge.

---
+++ SingleLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add("ONE");
+            list.Add("TWO");
+            list.Add("THREE");
+            list.Add("FOUR");
+            list.PrintList();
+
+            Console.ReadLine();
+        }
+    }
+}
