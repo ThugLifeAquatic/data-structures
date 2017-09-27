81d7a57df8c70c1dcf7de4cdd4ee5bd4c4c40ac6
Austin Cohrs <acohrsdev@outlook.com>
2017-09-26 17:54:09 -0700

    Challenge Completed

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QUEUE TEST:");
+            Queue testQ = new Queue();
+            testQ.Enqueue("ONE");
+            testQ.Enqueue("TWO");
+            testQ.Enqueue("THREE");
+            testQ.Enqueue("FOUR");
+            testQ.Dequeue();
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine("STACK TEST:");
+            Stack testS = new Stack();
+            testS.Push("ONE");
+            testS.Push("TWO");
+            testS.Push("THREE");
+            testS.Push("FOUR");
+            testS.Pop();
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ StacksAndQueues/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Stack
+    {
+        Node head;
+        Node tail;
+        int size;
+
+        public object Push(object data)
+        {
+            Node toAdd = new Node
+            {
+                Data = data
+            };
+            if (head == null)
+            {
+                head = toAdd;
+                tail = head;
+                Console.WriteLine($"Head: {head.Data} Tail: {tail.Data}");
+            }
+            else
+            {
+                tail.Next = toAdd;
+                toAdd.Prev = tail;
+                tail = toAdd;
+                Console.WriteLine($"Head: {head.Data} Tail: {tail.Data} TailPREV: {tail.Prev.Data}");
+                Console.WriteLine($"Head NEXT: {head.Next.Data}");
+
+
+            }
+            size++;
+            return toAdd.Data;
+        }
+
+        public object Pop()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("The Queue is empty.");
+                return null;
+            }
+            Node removed = tail;
+            tail = removed.Prev;
+            tail.Next = null;
+            Console.WriteLine($"Head: {head.Data} Tail: {tail.Data}");
+            size--;
+            return removed.Data;
+        }
+    }
+}
