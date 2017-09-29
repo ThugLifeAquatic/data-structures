26d177c58c548c9698e274c9ae59c4d3da0db08f
Austin Cohrs <acohrsdev@outlook.com>
2017-09-29 09:33:16 -0700

    Challenge Complete

---
+++ TwoStacks/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoStacks
+{
+    class Node
+    {
+        public object Data { get; set; }
+        public Node Prev { get; set; }
+        public Node Next { get; set; }
+    }
+}
---
+++ TwoStacks/StudentReadme.md
@@ -0,0 +1,7 @@
+﻿#ReadMe
+# Implementing a queue with two stacks
+ - Create 2 stacks
+ - Push into stack1
+ - Pop everything from 1 and push into 2
+ - Pop everything from 2 and push back into 1
+ - Return stack2.pop
