a5a027f93f8d18e2e068e91415e5a8b762e2a8a0
Austin Cohrs <acohrsdev@outlook.com>
2017-09-29 17:27:12 -0700

    Challenge Complete

---
+++ SortedStack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SortedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack:");
+            Stack testS = new Stack();
+            testS.Push("q");
+            testS.Push("w");
+            testS.Push("e");
+            testS.Push("r");
+            testS.Push("t");
+            testS.Push("y");
+            testS.Push("u");
+            testS.Push("i");
+            testS.Push("o");
+            testS.Push("p");
+            testS.Push("a");
+            testS.Push("s");
+            testS.Push("d");
+            testS.Push("f");
+            testS.Push("g");
+            testS.Push("h");
+            testS.Push("j");
+            testS.Push("k");
+            testS.Push("l");
+            testS.Push("z");
+            testS.Push("x");
+            testS.Push("c");
+            testS.Push("v");
+            testS.Push("b");
+            testS.Push("n");
+            testS.Push("m");
+            
+            Console.WriteLine("");
+
+            testS.SortPush("z");
+            
+            Console.Read();
+        }
+    }
+}
---
+++ SortedStack/StudentReadme.md
@@ -0,0 +1,9 @@
+﻿#ReadMe
+# Implementing a queue with two stacks
+## Use SortedPush() to add to and sort a stack.
+ - If Stack is empty, push new data into stack normally.
+ - Else, Add new data to empty list.
+ - Pop everything from stack and push into list.
+ - Sort list.
+ - For each item in list, push item into stack.
+ - Return data.
