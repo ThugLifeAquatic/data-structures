b780d6b57921dcf76789dd5e880bf1d7297001f8
Austin Cohrs <acohrsdev@outlook.com>
2017-10-02 18:01:48 -0700

    Challenge Complete

---
+++ Tree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoLevelTree
+{
+    class Node
+    {
+        public object Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node (object data)
+        {
+            Data = data;
+            Console.WriteLine($"Node {data}");
+        }
+    }
+}
