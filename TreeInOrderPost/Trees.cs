7d8d26c5560533a67955608a90e9524a14049e12
Austin Cohrs <acohrsdev@outlook.com>
2017-10-04 16:49:32 -0700

    Challenge Completed

---
+++ TreeInOrderPost/Trees.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeInorderPost
+{
+    class Trees
+    {
+        public Node Root { get; set; }
+
+        public object AddFirst(object data)
+        {
+            if (Root == null)
+            {
+                Root = new Node(data);
+                return Root.Data;
+            } else
+            {
+                Console.WriteLine("Tree already has a root");
+                return null;
+            }
+        }
+
+        //Incomplete Recursive add method(please ignore)
+
+        public void Traverse(Node curr)
+        {
+            if (curr!= null)
+            {
+                Console.WriteLine(curr.Data);
+                Traverse(curr.Left);
+                Traverse(curr.Right);
+            }
+        }
+
+        public void Inorder(Node curr)
+        {
+            if (curr != null)
+            {
+                Inorder(curr.Left);
+                Console.WriteLine(curr.Data);
+                Inorder(curr.Right);
+            }
+        }
+
+        public void Postorder(Node curr)
+        {
+            if (curr != null)
+            {
+                Postorder(curr.Left);
+                Postorder(curr.Right);
+                Console.WriteLine(curr.Data);
+            }
+        }
+
+    }
+}
