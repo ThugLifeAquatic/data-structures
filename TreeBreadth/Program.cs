2df710b310ab69163794cbc84120458ac47b589e
Austin Cohrs <acohrsdev@outlook.com>
2017-10-05 17:48:33 -0700

    Challenge Complete

---
+++ TreeBreadth/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TreeBreadth
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Tree World!");
+            Trees aTree = new Trees();
+            aTree.AddFirst(4);
+            aTree.Root.Left = new Node(2);
+            aTree.Root.Right = new Node(6);
+            aTree.Root.Left.Left = new Node(1);
+            aTree.Root.Left.Right = new Node(3);
+            aTree.Root.Right.Left = new Node(5);
+            aTree.Root.Right.Right = new Node(7);
+
+            Console.WriteLine("PREORDER:");
+
+            aTree.Traverse(aTree.Root);
+
+            Console.WriteLine("INORDER:");
+
+            aTree.Inorder(aTree.Root);
+
+            Console.WriteLine("POSTORDER:");
+
+            aTree.Postorder(aTree.Root);
+            Console.ReadLine();
+
+            Console.WriteLine("BREADTH:");
+
+            aTree.Breadth(aTree.Root);
+            Console.ReadLine();
+        }
+    }
+}
