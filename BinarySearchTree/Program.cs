937741d964d2a5b501df3368e6bd571a22ee03c7
Austin Cohrs <acohrsdev@outlook.com>
2017-10-16 19:05:30 -0700

    Completed binary serach tree add.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchTree testTree = new SearchTree();
+            testTree.Add(testTree.Root, 5);
+            testTree.Add(testTree.Root, 6);
+            testTree.Add(testTree.Root, 4);
+            testTree.Add(testTree.Root, 1);
+            Console.Read();
+        }
+    }
+}
