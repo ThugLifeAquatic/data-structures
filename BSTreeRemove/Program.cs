62372a13f17020268d79a597ec50e2dd1027bbdc
Austin Cohrs <acohrsdev@outlook.com>
2017-10-18 18:03:25 -0700

    Remove and findParent complete.

---
+++ BSTreeRemove/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BSTreeRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchTree testTree = new SearchTree();
+            testTree.Add(testTree.Root, 5);
+            testTree.Add(testTree.Root, 6);
+            testTree.Add(testTree.Root, 4);
+            testTree.Add(testTree.Root, 1);
+            testTree.Add(testTree.Root, 2);
+            testTree.Add(testTree.Root, 3);
+            testTree.Add(testTree.Root, 7);
+            testTree.Add(testTree.Root, 8);
+
+            Console.Write("MIN: ");
+            Console.WriteLine(testTree.Min(testTree.Root).Data);
+
+            Console.Write("MAX: ");
+            Console.WriteLine(testTree.Max(testTree.Root).Data);
+
+            Console.Write("FIND:");
+            Console.WriteLine(testTree.FindParent(testTree.Root, 10).Data);
+
+            Console.Write("REMOVE:");
+            Console.WriteLine(testTree.Remove(testTree.Root, 3).Data);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ BSTreeRemove/SearchTree.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSTreeRemove
+{
+    class SearchTree
+    {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public Node(int data)
+            {
+                Data = data;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public Node Root { get; set; }
+        public void Add(Node node, int data)
+        {
+            if (Root == null)
+            {
+                Root = new Node(data);
+                Console.WriteLine(Root.Data);
+                return;
+            }
+            if (node.Data == data)
+            {
+                return;
+            }
+            if (data < node.Data)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new Node(data);
+                    Console.WriteLine(node.Left.Data);
+                    return;
+                }
+                Add(node.Left, data);
+            }
+            if (data > node.Data)
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new Node(data);
+                    Console.WriteLine(node.Right.Data);
+                    return;
+                }
+                Add(node.Right, data);
+            }
+        }
+
+        public Node Min(Node node)
+        {
+            if(node == null)
+            {
+                return null;
+            }
+            if (node.Left == null)
+            {
+                return node;
+            }
+            return Min(node.Left);
+        }
+
+        public Node Max(Node node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            if (node.Right == null)
+            {
+                return node;
+            }
+            return Max(node.Right);
+        }
+
+        public Node FindParent (Node node, int data)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            if (data < node.Data && node.Left != null)
+            {
+                if (node.Left.Data == data)
+                {
+                    return node;
+                }
+                return FindParent(node.Left, data);
+            }
+            else if (data > node.Data && node.Right != null)
+            {
+                if (node.Right.Data == data)
+                {
+                    return node;
+                }
+                return FindParent(node.Right, data);
+            }
+            else
+            {
+                return new Node(0);
+            }
+
+        }
+
+        //Incomplete, Disregard
+
+        public Node Remove(Node node, int data)
+        {
+            Node Target;
+            Node Temp;
+            Node Swap;
+            Node Parent = FindParent(Root, data);
+            if (Parent.Left == null && Parent.Right == null)
+            {
+                return null;
+            }
+            if (Parent.Left != null && Parent.Left.Data == data)
+            {
+                Target = Parent.Left;
+                if (Target.Left == null)
+                {
+                    if (Target.Right == null)
+                    {
+                        Parent.Left = null;
+                        return Target;
+                    }
+                    Parent.Left = Target.Right;
+                }
+                Swap = Max(Target.Left);
+                Temp = FindParent(Target.Left, Swap.Data);
+                if (Swap.Left != null)
+                {
+                    Temp.Right = Swap.Left;
+                    Swap.Left = null;
+                }
+                Target.Data = Swap.Data;
+                Swap.Data = data;
+                return Swap;
+            }
+            else
+            {
+                Target = Parent.Right;
+                if (Target.Left == null)
+                {
+                    if (Target.Right == null)
+                    {
+                        Parent.Left = null;
+                        return Target;
+                    }
+                    Parent.Left = Target.Right;
+                }
+                Swap = Max(Target.Left);
+                Temp = FindParent(Target.Left, Swap.Data);
+                if (Swap.Left != null)
+                {
+                    Temp.Right = Swap.Left;
+                    Swap.Left = null;
+                }
+                Target.Data = Swap.Data;
+                Swap.Data = data;
+                return Swap;
+            }
+           
+
+
+        }
+    }
+}
