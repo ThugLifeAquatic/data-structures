91caeb4fd3693a586c8e5bfc349e767da89843ae
Austin Cohrs <acohrsdev@outlook.com>
2017-11-09 13:05:42 -0800

    Added a counter.

--- Possible combinations/Program.cs
+++ Possible combinations/Program.cs
@@ -4,60 +4,75 @@ namespace Possible_combinations
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
-            FindCombinations("abcd");
+            ComboCounter getDemCombos = new ComboCounter();
+            getDemCombos.FindCombinations("abcd");
             Console.Read();
         }
 
-        public static void FindCombinations(string theString)
+        public class ComboCounter
         {
-            Console.WriteLine(theString);
-            RecursiveScrambler(0, theString);
-        }
+            public int Count { get; set; }
 
-        public static void RecursiveScrambler(int start, string alphabet)
-        {
-            if (start < alphabet.Length)
+            public ComboCounter()
             {
-                char temp = alphabet[start];
-                char[] toArray = ToArray(alphabet);
-                for (int i = start; i < toArray.Length; i++)
+                Count = 1;
+            }
+
+            public void FindCombinations(string theString)
+            {
+
+                Console.WriteLine(theString);
+                RecursiveScrambler(0, theString);
+                Console.WriteLine(Count);
+            }
+
+            public void RecursiveScrambler(int start, string alphabet)
+            {
+                if (start < alphabet.Length)
                 {
-                    char[] printArray = ToArray(alphabet);
-                    printArray[start] = toArray[i];
-                    printArray[i] = temp;
-                    string newString = ArrToString(printArray);
-                    if (!newString.Contains(alphabet))
+                    char temp = alphabet[start];
+                    char[] toArray = ToArray(alphabet);
+                    for (int i = start; i < toArray.Length; i++)
                     {
-                        Console.WriteLine(newString);
-                    }
-                    if (start <= alphabet.Length)
-                    {
-                        RecursiveScrambler(start + 1, newString);
+                        char[] printArray = ToArray(alphabet);
+                        printArray[start] = toArray[i];
+                        printArray[i] = temp;
+                        string newString = ArrToString(printArray);
+                        if (!newString.Contains(alphabet))
+                        {
+                            Console.WriteLine(newString);
+                            Count++;
+                        }
+                        if (start <= alphabet.Length)
+                        {
+                            RecursiveScrambler(start + 1, newString);
+                        }
                     }
                 }
             }
-        }
 
-        public static char[] ToArray(string alphabet)
-        {
-            char[] toArray = new char[alphabet.Length];
-            for (int i = 0; i < alphabet.Length; i++)
+            public char[] ToArray(string alphabet)
             {
-                toArray[i] = alphabet[i];
+                char[] toArray = new char[alphabet.Length];
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    toArray[i] = alphabet[i];
+                }
+                return toArray;
             }
-            return toArray;
-        }
 
-        public static string ArrToString(char[] alphabet)
-        {
-            string newString = "";
-            for (int i = 0; i < alphabet.Length; i++)
+            public string ArrToString(char[] alphabet)
             {
-                newString += alphabet[i];
+                string newString = "";
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    newString += alphabet[i];
+                }
+                return newString;
             }
-            return newString;
         }
     }
 }
